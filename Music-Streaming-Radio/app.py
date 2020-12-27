7fcaebcf4ae21c83cfc669c5c755bfd311446029
Sungjin Jo <hv9448@gmail.com>
2020-12-28 02:32:08 +0900

    Simple music player added(All songs used royalty-free music from the YouTube Audio Library).

---
+++ Music-Streaming-Radio/app.py
@@ -0,0 +1,30 @@
+from flask import Flask, Response
+import socket
+from glob import glob
+import os
+
+song_list = sorted(glob(os.path.join('songs', '*.mp3')))
+
+app = Flask(__name__)
+
+def generate():
+    # mp3 파일을 읽어와서 계속 뿌려줌
+    for song_path in song_list:
+        with open(song_path,'rb') as f:
+            data = f.read(1024)
+            while data:
+                yield data
+                data = f.read(1024)
+
+@app.route('/')
+def stream():
+    return Response(generate(), mimetype = 'audio/mp3')
+
+if __name__ == '__main__':
+    PORT = 5000
+
+    with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
+        s.connect(('8.8.8.8', 80))
+        print('[*] Open http://%s:%s on your browser to enjoy music!' % (s.getsockname()[0], PORT))
+
+    app.run(host = '0.0.0.0', port = PORT)
