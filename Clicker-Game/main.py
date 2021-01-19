10949f595b27065db00e1d6df40463f022955033
JSJ <hv9448@gmail.com>
2021-01-19 13:52:50 +0900

    10000G까지 업그레이드 가능

--- Clicker-Game/main.py
+++ Clicker-Game/main.py
@@ -6,8 +6,8 @@ window.borderless = False
 window.color = color._20
 
 gold = 0
-gold_text = Text(text = str(gold), y = 0.2, scale = 2, background = True)
-button = Button(text = '1', color = color.light_gray, text_color = color.black, scale = .2)
+gold_text = Text(text = str(gold), y = .2, scale = 2, background = True)
+button = Button(text = '1', x = -.5, color = color.light_gray, text_color = color.black, scale = .2)
 
 def button_click():
     global gold
@@ -16,8 +16,6 @@ def button_click():
 button.on_click = button_click
 
 # auto gold generator
-auto_button_1 = Button(text = '1G/sec\n(Price: 10G)', x = 0.25, scale = 0.2, disabled = True, cost = 10, earn = 1)
-
 def auto_earn_gold(earn = 1, interval = 1):
     global gold
     gold += earn
@@ -31,20 +29,57 @@ def get_auto_gold(button, earn = 1):
         gold -= button.cost
         invoke(auto_earn_gold, earn = earn, interval = 1)
 
-#auto_button_1.on_click = get_auto_gold
-auto_button_1.on_click = Func(get_auto_gold, auto_button_1)
+auto_settings = [
+    {
+        'cost': 10,
+        'earn': 1,
+        'upgrade': 0,
+    },
+    {
+        'cost': 100,
+        'earn': 5,
+        'upgrade': 0,
+    },
+    {
+        'cost': 1000,
+        'earn': 25,
+        'upgrade': 0,
+    },
+    {
+        'cost': 10000,
+        'earn': 125,
+        'upgrade': 0,
+    },
+]
+
+auto_buttons = []
+
+for i, setting in enumerate(auto_settings):
+    b = Button(
+        text = f'{setting["earn"]}G/sec\n(Price: {setting["cost"]}G)',
+        x = .25 * (i + 1) - .5,
+        scale = 0.2,
+        disabled = True,
+        cost = setting['cost'],
+        earn = setting['earn'],
+        upgrade = setting['upgrade']
+    )
+
+    b.on_click = Func(get_auto_gold, b, b.earn)
+    auto_buttons.append(b)
 
 def update():
     global gold
 
     gold_text.text = str(gold)
-    if gold >= auto_button_1.cost:
-        auto_button_1.disabled = False
-        auto_button_1.color = color.green
-        auto_button_1.text_color = color.black
-    else:
-        auto_button_1.disabled = True
-        auto_button_1.color = color.black
-        auto_button_1.text_color = color.white
+    for button in auto_buttons:
+        if gold >= button.cost:
+            button.disabled = False
+            button.color = color.green
+            button.text_color = color.black
+        else:
+            button.disabled = True
+            button.color = color.black
+            button.text_color = color.white
 
 app.run()   # opens a window and starts the game.
